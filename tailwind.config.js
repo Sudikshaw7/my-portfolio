71b337baff535e6e93e0653fbc67414047c790c7
Sudiksha Wandre <sudikshawandre@gmail.com>
2025-06-13 20:48:41 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+      },
+      fontWeight: {
+        'extralight': 200,
+      },
+      letterSpacing: {
+        'widest': '0.2em',
+      },
+      animation: {
+        'pulse': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+      backdropBlur: {
+        'xs': '2px',
+      }
+    },
+  },
+  plugins: [],
+};
